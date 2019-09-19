05ae5d1385afcf46db203ae9a56e8e3ebcd80ce2
Matt Johnston <m@ttjohnston.com>
2019-09-18 21:57:33 -0300

    WIP - added routes and empty files

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,34 @@ import 'package:flutter/material.dart';
 import 'pages/goals_page.dart';
 import 'pages/registration/inputphone_page.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    final appTitle='ferda';
+    final appTitle = 'ferda';
 
     return MaterialApp(
-      title: appTitle,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
+        title: appTitle,
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        //group page maybe?
+        initialRoute:
+            '/goals', //to be mroe RESTful should this be something like '/group/[id]'?
+        routes: {
+          '/registration/phone': (ctx) => InputPhonePage(),
+          '/registration/group-name': (ctx) => InputGroupPage(),
+          '/registration/verification': (ctx) => InputVerificationCodePage(),
+          '/registration/invite': (ctx) => InviteUsers(),
+          '/goals': (ctx) => GoalsPage(),
+          '/goals/set': (ctx) => SetGoalPage(),
+          '/goals/show': (ctx) => ShowGoalPage(),
+        },
 
-      //group page maybe?
-      initialRoute: '/goals', //to be mroe RESTful should this be something like '/group/[id]'?
-      routes: {
-        '/login': (ctx) => InputPhone(),
-        '/goals': (ctx) => GoalsPage(),
-      },
+        //previsous code here built the scaffold Appbar, but I moved that logic into the GoalsPage return method
 
-      //previsous code here built the scaffold Appbar, but I moved that logic into the GoalsPage return method
-
-      home: GoalsPage()
-    );
+        home: GoalsPage());
   }
 }
--- lib/pages/goals/list_goals_page.dart
+++ lib/pages/goals/list_goals_page.dart
@@ -4,44 +4,35 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ferda/main.dart';
 
-class GoalsPage extends StatefulWidget{
-
-
-
+class GoalsPage extends StatefulWidget {
   @override
   _GoalsPageState createState() => _GoalsPageState();
 }
 
 class _GoalsPageState extends State<GoalsPage> {
-
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     final myFuture = isSignedIn();
 
-    myFuture.then((status){
-      if(status== false)
+    myFuture.then((status) {
+      if (status == false)
 
         //its still showing back button
         // TODO: is there a way to replace page, rather than "push"
-        Navigator.
-        pushReplacementNamed(context, '/login');
-    }
-    );
+        Navigator.pushReplacementNamed(context, '/login');
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-
     //I wonder if I can put logic here, as the widget is building? try
     print('in goals build');
 
-      
-    
     //placeholder text widget until implement this page
 
-    // actually, kinda weird to wrap this in scaffold, because when page first loads, it 
+    // actually, kinda weird to wrap this in scaffold, because when page first loads, it
 
     return Scaffold(
         appBar: AppBar(
@@ -50,27 +41,24 @@ class _GoalsPageState extends State<GoalsPage> {
         ),
         body: Center(
           child: Text("List of goals of the group displayed here."),
-        )
-      );
+        ));
 
     // return Center(
     //   child: Text("List of goals of the group displayed here."),
     // );
   }
 
-  Future<bool> isSignedIn() async{
-
+  Future<bool> isSignedIn() async {
     //when I run with the below line, should make me go to register screen
     FirebaseAuth.instance.signOut();
 
     //am I using await / future / async correctly? why are they needed, what would happen if removed.
 
-    // Try without try catch block.. curious if works. 
+    // Try without try catch block.. curious if works.
 
-    try{
-      if (await FirebaseAuth.instance.currentUser() != null)
-        return true;
-    } catch(e){
+    try {
+      if (await FirebaseAuth.instance.currentUser() != null) return true;
+    } catch (e) {
       print(e.message);
     }
 
